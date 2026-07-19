dccc5f6224a780887fe2a3f3daec2668d205fd5c
Ashton Mathias <mathiasashton07@gmail.com>
2026-07-19 22:59:25 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@LinkTrim/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { createDb } from "@LinkTrim/db";
+import * as schema from "@LinkTrim/db/schema/auth";
+import { env } from "@LinkTrim/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [nextCookies()],
+  });
+}
+
+export const auth = createAuth();
